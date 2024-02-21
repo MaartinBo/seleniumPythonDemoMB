fc8fd08429e8e3ade99432e41aa938e180c0587e
MaartinBo <bigben963@gmail.com>
2024-02-21 22:03:48 +0100

    Add headless by default

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,20 +1,22 @@
-from selenium.webdriver.chrome import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from webdriver_manager.firefox import GeckoDriverManager
-
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 class DriverFactory:
     @staticmethod
     def get_driver(browser):
         if browser == "chrome":
-            options = webdriver.ChromeOptions()
-            options.add_experimental_option("detach", True)
+            options = Options()
+            options.add_argument("--headless")
+            options.add_argument("--disable-dev-shm-usage")
+            options.add_argument("--no-sandbox")
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1920,1080")
             options.add_experimental_option('excludeSwitches', ['enable-logging'])
-            options.add_argument("start-maximized")
             return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
         elif browser == "firefox":
             options = webdriver.FirefoxOptions()
+            options.headless = True
             return webdriver.Firefox(service=Service(GeckoDriverManager().install()), options=options)
         raise Exception("Provide valid driver name")
