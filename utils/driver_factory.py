561ddfd3ee94fd36ce046a037b3484431945759f
MaartinBo <bigben963+github@gmail.com>
2024-09-16 21:23:29 +0200

    fix: update Firefox driver logging and headless options

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -1,14 +1,19 @@
+import os
+
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 
 class DriverFactory:
     @staticmethod
     def get_driver(browser):
         if browser == "chrome":
-            options = Options()
+            options = ChromeOptions()
             options.add_argument("--headless")
             options.add_argument("--disable-dev-shm-usage")
             options.add_argument("--no-sandbox")
@@ -16,12 +21,15 @@ class DriverFactory:
             options.add_argument("--window-size=1920,1080")
             options.add_experimental_option("excludeSwitches", ["enable-logging"])
             return webdriver.Chrome(
-                service=Service(ChromeDriverManager().install()), options=options
+                service=ChromeService(ChromeDriverManager().install()), options=options
             )
         elif browser == "firefox":
-            options = webdriver.FirefoxOptions()
-            options.headless = True
-            return webdriver.Firefox(
-                service=Service(GeckoDriverManager().install()), options=options
+            options = FirefoxOptions()
+            options.add_argument("-headless")
+            service = FirefoxService(
+                GeckoDriverManager().install(),
+                log_output=os.devnull #ignore firefox logs
             )
+
+            return webdriver.Firefox(service=service, options=options)
         raise Exception("Provide valid driver name")
